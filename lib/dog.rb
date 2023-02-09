1e326b6ba0447ad33908634357b89bf150201c54
yurubahmed <yurubiftin2002@gmail.com>
2023-02-09 18:02:02 -0500

    passes all tests

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
 
+  def name= (name)  
+    @name= name
+  end
+
+  def name
+    @name
+  end
+
+  def breed= (breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
 end
+
+fido = Dog.new
+fido.name=("fido")
+
+fido.breed=("pointer")
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
+    def name= (name)
+        @name = name
+    end
+    
+    def name
+        @name
+    end
+
+    def job= (job)
+        @job = job
+    end
+    def job
+        @job
+    end
+
 
 end
+
+yurub= Person.new
+yurub.name ="yurub"
+
+yurub.job= "software engineering"
